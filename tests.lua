b99794b512bb2876489bdcc13f3ff36e80dd1dcb
Gajos <thegajos@gmail.com>
2020-01-22 22:06:34 +0100

    update Gaios & TAE

--- tests.lua
+++ tests.lua
@@ -45,69 +45,85 @@ end
 
 
 
-local mob1 = {
-    id = 1,
-    x = 0,
-    y = 0,
-    name = 'Gajos',
-}
-local mob2 = {
-    id = 2,
-    x = 1,
-    y = 1,
-    name = 'TAE',
-}
-local mob3 = {
-    id = 3,
-    x = 1,
-    y = 0,
-    name = 'Kebab',
-}
-local mob4 = {
-    id = 4,
-    x = 0,
-    y = 0,
-    name = 'Cola',
-}
-local mob5 = {
-    id = 5,
-    x = 3,
-    y = 7,
-    name = 'Pepsi',
-}
+local Mob = {}
+local MOBS = {}
+function Mob:new(name, x, y)
+	local o = {}
+    setmetatable(o, self)
+    self.__index = self
 
-local chunk = Map:getChunkById(7)
-chunk:addToCollection('mobs', mob1)
-chunk:addToCollection('mobs', mob2)
-chunk:addToCollection('mobs', mob3)
-chunk:addToCollection('mobs', mob4)
-chunk:addToCollection('mobs', mob5)
+	Map:initEntity('mobs', o, x, y)
 
-for _, mob in pairs(chunk.collections['mobs']) do
-    print(mob.id, mob.name)
+    o.name = name
+	o.image_id = image('gfx/block.bmp', x * 32 + 16, y * 32 + 16, 1)
+    -- imagescale(o.image_id, 0.7, 0.7)
+
+    MOBS[#MOBS + 1] = o
+
+	return o
 end
 
---Removing from collection
-chunk:removeFromCollection('mobs', {name = 'Cola'})
+function Mob:getX()
+    return object(self.image_id, 'tilex')
+end
 
---Expected result: 1, 2, 3, 5
+function Mob:getY()
+    return object(self.image_id, 'tiley')
+end
 
+function Mob:setPos(x, y, time)
+    local new_x, new_y = x * 32 + 16, y * 32 + 16
 
-addhook('movetile', 'onMovetile')
-function onMovetile(id, tilex, tiley)
-    local chunk = Map:getChunkAt(tilex, tiley)
-
-    local str = 'chunk_id: '..chunk.id..'('
-    for _, nchunks in pairs(chunk.neighbours) do
-        str = str..nchunks.id..','
+    if not time then
+    	imagepos(self.image_id, new_x, new_y, 0)
+    else
+        tween_move(self.image_id, time, new_x, new_y)
     end
-    str = str..')'
-    str = str..' x: '..chunk.x..' y: '..chunk.y
-    str = str..' collection('
-    local mobs = chunk:getFromCollection('mobs')
-    for _, mob in pairs(mobs) do
-        str = str..mob.id..','
+end
+
+function Mob:onMovetile(x, y)
+	Map:updateEntity(self, x, y)
+end
+
+addhook('second', 'onSecond')
+function onSecond()
+    for i = 1, #MOBS do
+        local mob = MOBS[i]
+        local x, y = mob:getX(), mob:getY()
+
+        x = x + 1
+		-- if (i == 2) then
+		-- 	y = y + 1
+		-- end
+
+        mob:onMovetile(x, y)
+        mob:setPos(x, y, 1000)
     end
-    str = str..')'
-    hudtxt2(id, 1, str, 50, 300, 0, 0, 20)
+end
+
+local mob1 = Mob:new('Gaios', 6, 6)
+local mob2 = Mob:new('TAE', 4, 8)
+
+
+addhook('ms100', 'onMs100')
+function onMs100()
+    for _, id in pairs(player(0, 'tableliving')) do
+        local tilex, tiley = player(id, 'tilex'), player(id, 'tiley')
+	    local chunk = Map:getChunkAt(tilex, tiley)
+
+	    local str = 'chunk_id: '..chunk.id..'('
+	    for _, nchunks in pairs(chunk.neighbours) do
+	        str = str..nchunks.id..','
+	    end
+	    str = str..')'
+	    str = str..' x: '..chunk.x..' y: '..chunk.y
+	    local str2 = 'collection('
+	    local mobs = chunk:getFromCollection('mobs', nil, true)
+	    for _, mob in pairs(mobs) do
+	        str2 = str2..mob.name..','
+	    end
+	    str2 = str2..')'
+        hudtxt2(id, 1, str, 50, 300, 0, 0, 20)
+	    hudtxt2(id, 2, str2, 50, 330, 0, 0, 20)
+	end
 end
